af7625bd4e021887913531203d4ae3736413eede
osboxes.org <osboxes@manager.vanilla.com>
2019-11-29 20:59:19 +0100

    CORS

--- LinkedIn_server.py
+++ LinkedIn_server.py
@@ -388,7 +388,6 @@ class Request(http.server.SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type","application/json")
             self.send_header("Access-Control-Allow-Origin", "*")
-            self.send_header("Access-Control-Allow-Origin","*")
             self.send_header("Access-Control-Allow-Methods","GET,POST,OPTIONS")
             self.send_header("Access-Control-Allow-Headers","X-Requested-With")
             self.end_headers()
