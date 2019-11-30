6482c49028560bac225945b7a367f28db563dbbe
osboxes.org <osboxes@manager.vanilla.com>
2019-11-30 19:33:31 +0100

    Upload photo

--- script/applicants.js
+++ script/applicants.js
@@ -1,7 +1,7 @@
 const register = async () => {
   let linkedinEmail = document.getElementById('linkedinEmail').value;
-  let twitterUsername = document.getElementById('twitterUsername').value;
-
+  let twitterUsername = await document.getElementById('twitterUsername').value;
+  window.localStorage.setItem('twitterUsername',twitterUsername);
 
   let response = await fetch('http://3.227.193.57:8001/applicants');
   let data = await response.json();
@@ -39,3 +39,50 @@ const register = async () => {
 	  })
     }
   };
+
+//convert image to Base64
+function getBase64(file,cb){
+  var reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = function(){
+    console.log(reader.result);
+    cb(reader.result);
+  };
+  reader.oneerror = function (error) {
+    console.log('Error: ', error);
+  }
+}
+
+function uploadPhoto(data) {
+    $.ajax({
+        url: 'http://3.227.193.57:8001/users/accounts/photo/'+localStorage.getItem("twitterUsername"),
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        dataType: 'json',
+        success: function(data){
+            console.log("success");
+            //self.location = "http://localhost:4007/";
+        },
+        error: function(xhr,status,error){
+            console.log("error");
+            console.log(error);
+        }
+    });
+}
+
+const uploadButton = async() => {
+  var files = await document.getElementById("foto").files;
+  var data = {};
+  if (files.length > 0){
+    getBase64(files[0], function(result){
+      data.Photo = result;
+      console.log(data.Photo);
+      uploadPhoto(data);
+      console.log(data);
+    })
+  }
+  else {
+    uploadPhoto(data);
+  }
+}
